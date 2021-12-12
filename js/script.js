e03dc82a9038834b5cf997fb50a7b18182f90ce0
Kirill <kirillkolesnichenko21@gmail.com>
2021-12-12 12:58:48 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    // Wow js
+    wow = new WOW({
+        animateClass: 'animate__animated',
+    })
+    wow.init();
+
+    // Scroll
+    $(".toHome").click(function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $(".hero").offset().top
+        }, 200);
+    });
+
+    $(".toFeatured").click(function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $(".featured").offset().top
+        }, 200);
+    });
+
+    $(".toService").click(function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $(".service").offset().top
+        }, 200);
+    });
+
+    $(".toPricing").click(function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $(".pricing").offset().top
+        }, 200);
+    });
+})
